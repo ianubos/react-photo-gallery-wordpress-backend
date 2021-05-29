dd661cfa9e285cd680db60199060cd44cd51ba12
ianubos <1031gonshi@gmail.com>
2021-05-30 00:41:27 +0900

    data completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+import useFetch from "./hooks/useFetch";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
@@ -6,23 +8,91 @@ import Contact from "./pages/Contact";
 import ScrollToTop from "./hooks/useScrollTop";
 import sitedata from "./siteData";
 
+const noneeds = ["archive", "top", "sample-page"];
+
+function dataCleaner(media, posts) {
+  if (!posts || !media) return;
+  return posts.map((post) => {
+    const target_media = media.filter((m) => m.id === post.featured_media)[0];
+    return {
+      image: target_media.source_url,
+      thumbnail: target_media.media_details.sizes.thumbnail.source_url,
+      alt: target_media.title.rendered,
+    };
+  });
+}
+
 function App() {
   const slugs = Object.entries(sitedata().pages).map((arr) => {
     return arr[0];
   });
+  const [latest, setLatest] = useState(undefined);
+  const [categories, setCategories] = useState(undefined);
+  const [categoryNumber, setCategoryNumber] = useState({});
+  const url = "http://tsuyu-photogallery.com/wp-json/wp/v2";
+  const posts = useFetch(`${url}/posts`);
+  const media = useFetch(`${url}/media`);
+  const pages = useFetch(`${url}/pages`);
+  const cats = useFetch(`${url}/categories`);
+  useEffect(() => {
+    if (cats) {
+      let tmp = {};
+      cats.forEach((cat) => {
+        tmp[cat.id] = cat.slug;
+      });
+      setCategoryNumber(tmp);
+    }
+    // eslint-disable-next-line
+  }, [cats]);
+  useEffect(() => {
+    if (posts && media) {
+      setLatest(
+        dataCleaner(media, posts.slice(0, 3))
+      );
+    }
+    // eslint-disable-next-line
+  }, [posts, media]);
+  useEffect(() => {
+    if (pages && media) {
+      setCategories(
+        pages
+          .filter((page) => !noneeds.includes(page.slug))
+          .map((page) => {
+            return {
+              title: page.title.rendered,
+              slug: page.slug,
+              image: media.filter((m) => m.id === page.featured_media)[0]
+                .source_url,
+            };
+          })
+      );
+    }
+    // eslint-disable-next-line
+  }, [pages, media]);
   return (
     <Router>
       <Header />
       <ScrollToTop />
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home latest={latest} categories={categories} />
         </Route>
-        {slugs.map((slug) => (
-          <Route path={`/${slug}`}>
-            <Museum />
-          </Route>
-        ))}
+        {posts &&
+          slugs &&
+          slugs.map((slug) => (
+            <Route path={`/${slug}`}>
+              <Museum
+                data={dataCleaner(
+                  media,
+                  posts.filter((post) =>
+                    post.categories
+                      .map((cat) => categoryNumber[cat])
+                      .includes(slug)
+                  )
+                )}
+              />
+            </Route>
+          ))}
         <Route path="/contact">
           <Contact />
         </Route>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
-import useFetch from "../hooks/useFetch";
 import BackPicture from "../components/BackPicture";
 import MainContent from "../components/MainContent";
 
-const noneeds = ["archive", "top", "sample-page"];
-
-export default function Home() {
-  const [latest, setLatest] = useState(undefined);
-  const [categories, setCategories] = useState(undefined);
-  const url = "http://tsuyu-photogallery.com/wp-json/wp/v2";
-  const posts = useFetch(`${url}/posts`);
-  const media = useFetch(`${url}/media`);
-  const pages = useFetch(`${url}/pages`);
-  useEffect(() => {
-    if (posts && media) {
-      setLatest(
-        posts.slice(0, 3).map((post) => {
-          const target_media = media.filter(
-            (m) => m.id === post.featured_media
-          )[0];
-          return {
-            image: target_media.source_url,
-            thumbnail: target_media.media_details.sizes.thumbnail.source_url,
-            alt: target_media.title.rendered,
-          };
-        })
-      );
-      console.log("latestPosts:", latest);
-    }
-    // eslint-disable-next-line
-  }, [posts, media]);
-  useEffect(() => {
-    if (pages && media) {
-      setCategories(
-        pages
-          .filter((page) => !noneeds.includes(page.slug))
-          .map((page) => {
-            return {
-              title: page.title.rendered,
-              slug: page.slug,
-              image: media.filter((m) => m.id === page.featured_media)[0]
-                .source_url,
-            };
-          })
-      );
-    }
-    // eslint-disable-next-line
-  }, [pages, media]);
+export default function Home({latest, categories}) {
   return (
     <div>
       <Helmet>
